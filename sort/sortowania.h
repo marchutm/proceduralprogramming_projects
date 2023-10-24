9d395a4a7b2fbce053b234805ecbd44ec100e63b
marchutm <117740530+marchutm@users.noreply.github.com>
2023-10-24 23:17:21 +0200

    Add files via upload

---
+++ sort/sortowania.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H_
+#define SORT_H_
+
+void simpleInsertion( int* pTab, int nSize );
+void simpleSelectionSort( int* pTab, int nSize );
+void bubbleSort( int* pTab, int nSize );
+void mixedBubbleSort( int* pTab, int nSize );
+void halfSort( int* pTab, int nSize );
+void heapSort( int* pTab, int nSize );
+void quickSort( int* pTab, int nSize );
+
+
+#endif
